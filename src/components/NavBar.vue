d5459a5e1bfdc9caeea002abd4e6c660fa6b3555
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-02-15 13:34:18 +0100

    Create NavBar component
    Also add base structure for the component and its name.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav" class="nav">
+    <router-link to="/">Real World Events</router-link>
+    <nav>
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style scoped>
+</style>
