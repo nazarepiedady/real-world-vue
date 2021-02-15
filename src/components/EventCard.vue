28e868dcb0955377fde49a86817e70eb3f6a968e
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-02-15 13:44:48 +0100

    Update EventCard component
    Define the data structure used by EventCard component
    
    Set styles rules to the component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,55 @@
 <template>
-  <div></div>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: '1' } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </router-link>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Aug 19, 2018',
+        time: '6:00',
+        attendees: [
+          { id: 'abc123', name: 'Adam Jahr' },
+          { id: 'def456', name: 'Gregg Pollack' },
+        ],
+      },
+    };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  cursor: pointer;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  font-weight: 100;
+  text-decoration: none;
+}
+</style>
